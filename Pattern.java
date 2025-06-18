854a1d92d1e9e7daf058517cf39052204c241137
samudraladinesh <157967858+samudraladinesh@users.noreply.github.com>
2025-06-18 12:41:14 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -98,4 +98,18 @@ System.out.println();
 }
 }*/
 
+/*class Pattern{
+public static void main(String[]args){
+for(int i=1;i<=5;i++){
+for(int j=1;j<=5-i;j++){
+System.out.print(" ");
+}
+for(int k=1;k<=i;k++){
+System.out.print((2*k) + " ");
+}
+System.out.println();
+}
+}
+}*/
+
 
