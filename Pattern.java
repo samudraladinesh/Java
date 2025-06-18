50891f01f847cd8d86545be9aa92f0333a50c44e
samudraladinesh <157967858+samudraladinesh@users.noreply.github.com>
2025-06-18 12:36:18 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,101 @@
+/*class Pattern
+{
+public static void main(String [] args)
+{
+for(int i=0;i<6;i++)
+{
+for(int j=0;j<6;j++)
+{
+ System.out.print("* ");
+}
+System.out.println();
+}
+}
+}*/
+
+/* class Pattern
+{
+public static void main(String [] args)
+{
+  for(int i=1;i<6;i++)
+{
+for(int j=0;j<5;j++)
+{
+ System.out.print(i+" ");
+}
+System.out.println();
+}
+}
+} 
+*/
+
+/*class Pattern
+{
+public static void main(String [] args)
+{
+  for(int i=1;i<6;i++)
+{
+for(int j=1;j<6;j++)
+{
+ System.out.print(j+" ");
+}
+System.out.println();
+}
+}
+}*/
+
+	
+/*class Pattern{
+public static void main(String[] args){
+int n=1;
+for(int  i=1;i<=5;i++)
+{
+for(int j=1;j<=5;j++)
+{
+System.out.print(n++ + " ");
+}
+System.out.println();
+}
+}
+}*/
+
+/*class Pattern{
+public static void main(String []args){
+int n=1;
+for(int i=1;i<=5;i++){
+for(int j=0;j<5;j++)
+{
+System.out.print(n + " ");
+n=n+2;
+}
+System.out.println();
+}
+}
+}*/
+
+/*class Pattern{
+public static void main(String []args){
+for(int i=0;i<5;i++){
+for(int j=0;j<5;j++){
+System.out.print((j%2==0)?"1":"0");
+}
+System.out.println();
+}
+}
+}*/
+
+/*class Pattern{
+public static void main(String[]args){
+for(int i=1;i<=5;i++){
+for(int j=1;j<=5-i;j++){
+System.out.print(" ");
+}
+for(int k=1;k<=i;k++){
+System.out.print("*" +" ");
+}
+System.out.println();
+}
+}
+}*/
+
+
