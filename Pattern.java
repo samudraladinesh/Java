21bbd48ae8e7375f487b3da1848907064113325a
samudraladinesh <157967858+samudraladinesh@users.noreply.github.com>
2025-06-18 13:03:13 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -183,3 +183,32 @@ System.out.println();
 }
 }
 }*/
+
+
+/*class Pattern {
+    public static void main(String[] args) {
+        int n = 4;
+for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++)
+                System.out.print(" ");
+            System.out.print("*");
+           if (i != 0) {
+                for (int j = 0; j < 2 * i - 1; j++)
+                    System.out.print(" ");
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++)
+                System.out.print(" ");
+             System.out.print("*");
+            if (i != 0) {
+                for (int j = 0; j < 2 * i - 1; j++)
+                    System.out.print(" ");
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}*/
